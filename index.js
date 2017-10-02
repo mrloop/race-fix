12373af6dd3f43b921d02d83e25c5309144a275b
mrloop <ewan@mrloop.com>
2017-10-02 21:23:23 +0100

    Helper method to inject fixutres

--- index.js
+++ index.js
@@ -5,4 +5,13 @@ import event_with_entrants_html from './fixtures/event_with_entrants.html';
 import event_with_results_html from './fixtures/event_with_results.html';
 import person_html from './fixtures/person.html';
 
+export function injectFixtures(obj) {
+  obj.inject('event_with_entrants_html', event_with_entrants_html);
+  obj.inject('events_html', events_html);
+  obj.inject('person_html', person_html);
+  obj.inject('entrants_html', entrants_html);
+}
+
 export { entrants_html, event_html, events_html, event_with_entrants_html, event_with_results_html, person_html }
+
+
